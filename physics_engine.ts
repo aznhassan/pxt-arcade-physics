c90c41790ee81d2ab43e852639b6c792bfa28a04
Hassan Sufi <hasufi@microsoft.com>
2021-08-19 14:51:42 -0600

    Create a new physic engine class that includes momentum

---
+++ physics_engine.ts
@@ -0,0 +1,18 @@
+class ArcadePhysicsEnginePlus extends ArcadePhysicsEngine {
+    protected _maxMomentum: Fx8;
+    
+    constructor(maxVelocity: number, minSingleStep: number, maxSingleStep: number, maxMomentum: 500) {
+        super(maxVelocity, minSingleStep, maxSingleStep);
+        this.maxMomentum = maxMomentum;
+        console.log("Yo");
+    }
+
+    get maxMomentum(): number {
+        return Fx.toInt(this._maxMomentum);
+    }
+
+    set maxMomentum(maxMomentum: number) {
+        this._maxMomentum = Fx8(maxMomentum)
+    }
+}
+
