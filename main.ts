8a8edef7583fd90e2952afd1a9256cfa13a1dbcc
Hassan Sufi <hasufi@microsoft.com>
2021-08-20 16:04:50 -0600

    Add blocks to set the mass of an object

--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
+namespace sprites {
+    /**
+     * Sets the mass of a sprite
+     */
+    //% blockId=setMass block="set $sprite=variables_get mass to number $value"
+    //% group="Physics"
+    //% weight=10
+    //% blockGap=8
+    export function setMass(sprite: Sprite, value: number) {
+        setDataNumber(sprite, 'mass', value);
+    }
+
+    /**
+     * Gets the mass of a sprite
+     */
+    //% blockId=spriteDataGetNumber block="$sprite=variables_get mass"
+    //% group="Physics"
+    //% weight=10
+    //% blockGap=8
+    export function getMass(sprite: Sprite): number {
+        return readDataNumber(sprite, 'mass');
+    }
+
+    /**
+     * Gets a number in the data of a sprite
+     */
+    function readDataNumber(sprite: Sprite, name: string): number {
+        if (!sprite || !name) return 0;
+        const d = sprite.data;
+        return d[name] as number;
+    }
+
+    /**
+     * Sets a number in the data of a sprite
+     */
+    function setDataNumber(sprite: Sprite, name: string, value: number) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d['mass'] = value;
+    }
+}
+
 function transferPhysicsEngine() {
     let newPhysicsEngine = new ArcadePhysicsEnginePlus(500, 2, 4, 500)
     game.setPhysicsEngine(newPhysicsEngine)
--- physics_engine.ts
+++ physics_engine.ts
@@ -1,5 +1,10 @@
+/**
+ * An upgraded Physics Engine that includes drag
+*/
 class ArcadePhysicsEnginePlus extends ArcadePhysicsEngine {
     protected _maxMomentum: Fx8;
+    protected readonly airDensity = Fx8(1)
+    protected readonly airFlowVelocity = Fx8(10)
     
     constructor(maxVelocity: number, minSingleStep: number, maxSingleStep: number, maxMomentum: 500) {
         super(maxVelocity, minSingleStep, maxSingleStep);
