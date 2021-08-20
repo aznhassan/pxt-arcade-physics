37d78772f30a20ac36dded7bb810d7c380ea09f9
Hassan Sufi <hasufi@microsoft.com>
2021-08-20 15:18:29 -0600

    Set the controller to retain sprite velocity on no input

--- main.ts
+++ main.ts
@@ -5,12 +5,16 @@ function transferPhysicsEngine() {
 
 let sceneChangeHandler = (oldScene: scene.Scene) => {
     transferPhysicsEngine()
+    // Don't zero out the velocity when there is no more input from the controller,
+    // instead let gravity && drag handle slowing down moving objects
+    controller.setRetainVelocityOnNoInput(true);
 }
 
 console.log("[HASUFI] HELLO THERE")
 
-// Replace the old physics engine with the new one
 transferPhysicsEngine()
+controller.setRetainVelocityOnNoInput(true);
+
 console.log("[HASUFI] Replaced the old physics engine with a new one")
 
 // Add the scene handler to replace the physics engine when the scene is re-created
